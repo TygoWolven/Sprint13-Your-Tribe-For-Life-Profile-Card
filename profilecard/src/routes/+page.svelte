e2e3dd3d3e9715fab434dc116d0859030514b346
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2024-09-08 13:33:38 +0200

    Created Svelte Skeleton

---
+++ profilecard/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<section id="home">
+    <h2>Wolven Tygo Wolven</h2>
+    <h2>Tygo Wolven Tygo</h2>
+    <p>Frontend Developer</p>
+
+    <a href="#contact">Scroll</a>
+</section>
+
+<section id="contact">
+    <ul>
+        <li>Github</li>
+        <li>Instagram</li>
+    </ul>
+</section>
+
+
+<style>
+    h2 {
+        text-transform: uppercase;
+    }
+</style>
