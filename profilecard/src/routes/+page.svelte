ab4aef7b4befdf5c0f9a417f7a140dfb59beefd9
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2024-10-27 16:03:45 +0100

    Added socials page.

--- profilecard/src/routes/+page.svelte
+++ profilecard/src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script>
-    import trainTrack from "$lib/assets/trainTrack.png"
-    import mineCart from "$lib/assets/mineCart.png"
 </script>
 
 <section id="home">
@@ -19,25 +17,57 @@
 </section>
 
 <section id="contact">
-    <img alt="The project logo" src={mineCart} />
-    <img alt="The project logo" src={trainTrack} />
+    <h2>Socials</h2>
+    <div class="wrapper">
+        <div class="container">
+            <input type="radio" name="slide" id="c1" checked >
+            <label for="c1" class="card">
+                <div class="row">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0 0 64 64">
+                            <path d="M32,6C17.641,6,6,17.641,6,32c0,14.359,11.641,26,26,26s26-11.641,26-26C58,17.641,46.359,6,32,6z M25,44h-5V26h5V44z M22.485,24h-0.028C20.965,24,20,22.888,20,21.499C20,20.08,20.995,19,22.514,19c1.521,0,2.458,1.08,2.486,2.499 C25,22.887,24.035,24,22.485,24z M44,44h-5v-9c0-3-1.446-4-3-4c-1.445,0-3,1-3,4v9h-5V26h5v3c0.343-0.981,1.984-3,5-3c4,0,6,3,6,8 V44z"></path>
+                        </svg>
+                    </div>
+                    <div class="description">
+                        <h3>Linkedin</h3>
+                        <a href="https://www.linkedin.com/in/tygo-wolven-091515241/">Tygo Wolven</a>
+                    </div>
+                </div>
+            </label>
+            <input type="radio" name="slide" id="c2" >
+            <label for="c2" class="card">
+                <div class="row">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 30 30">
+                            <path d="M 15 2 C 7.832 2 2 7.832 2 15 C 2 22.168 7.832 28 15 28 C 22.168 28 28 22.168 28 15 C 28 7.832 22.168 2 15 2 z M 11.666016 6 L 18.332031 6 C 21.457031 6 24 8.5420156 24 11.666016 L 24 18.332031 C 24 21.457031 21.457984 24 18.333984 24 L 11.667969 24 C 8.5429688 24 6 21.457984 6 18.333984 L 6 11.667969 C 6 8.5429688 8.5420156 6 11.666016 6 z M 11.666016 8 C 9.6450156 8 8 9.6459688 8 11.667969 L 8 18.333984 C 8 20.354984 9.6459688 22 11.667969 22 L 18.333984 22 C 20.354984 22 22 20.354031 22 18.332031 L 22 11.666016 C 22 9.6450156 20.354031 8 18.332031 8 L 11.666016 8 z M 19.667969 9.6660156 C 20.035969 9.6660156 20.333984 9.9640312 20.333984 10.332031 C 20.333984 10.700031 20.035969 11 19.667969 11 C 19.299969 11 19 10.700031 19 10.332031 C 19 9.9640312 19.299969 9.6660156 19.667969 9.6660156 z M 15 10 C 17.757 10 20 12.243 20 15 C 20 17.757 17.757 20 15 20 C 12.243 20 10 17.757 10 15 C 10 12.243 12.243 10 15 10 z M 15 12 A 3 3 0 0 0 15 18 A 3 3 0 0 0 15 12 z"></path>
+                        </svg>
+                    </div>
+                    <div class="description">
+                        <h3>Instagram</h3>
+                        <a href="https://www.instagram.com/tygowolvenn_/">tygowolvenn_</a>
+                    </div>
+                </div>
+            </label>
+            <input type="radio" name="slide" id="c3" >
+            <label for="c3" class="card">
+                <div class="row">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 50 50">
+                            <path d="M17.791,46.836C18.502,46.53,19,45.823,19,45v-5.4c0-0.197,0.016-0.402,0.041-0.61C19.027,38.994,19.014,38.997,19,39 c0,0-3,0-3.6,0c-1.5,0-2.8-0.6-3.4-1.8c-0.7-1.3-1-3.5-2.8-4.7C8.9,32.3,9.1,32,9.7,32c0.6,0.1,1.9,0.9,2.7,2c0.9,1.1,1.8,2,3.4,2 c2.487,0,3.82-0.125,4.622-0.555C21.356,34.056,22.649,33,24,33v-0.025c-5.668-0.182-9.289-2.066-10.975-4.975 c-3.665,0.042-6.856,0.405-8.677,0.707c-0.058-0.327-0.108-0.656-0.151-0.987c1.797-0.296,4.843-0.647,8.345-0.714 c-0.112-0.276-0.209-0.559-0.291-0.849c-3.511-0.178-6.541-0.039-8.187,0.097c-0.02-0.332-0.047-0.663-0.051-0.999 c1.649-0.135,4.597-0.27,8.018-0.111c-0.079-0.5-0.13-1.011-0.13-1.543c0-1.7,0.6-3.5,1.7-5c-0.5-1.7-1.2-5.3,0.2-6.6 c2.7,0,4.6,1.3,5.5,2.1C21,13.4,22.9,13,25,13s4,0.4,5.6,1.1c0.9-0.8,2.8-2.1,5.5-2.1c1.5,1.4,0.7,5,0.2,6.6c1.1,1.5,1.7,3.2,1.6,5 c0,0.484-0.045,0.951-0.11,1.409c3.499-0.172,6.527-0.034,8.204,0.102c-0.002,0.337-0.033,0.666-0.051,0.999 c-1.671-0.138-4.775-0.28-8.359-0.089c-0.089,0.336-0.197,0.663-0.325,0.98c3.546,0.046,6.665,0.389,8.548,0.689 c-0.043,0.332-0.093,0.661-0.151,0.987c-1.912-0.306-5.171-0.664-8.879-0.682C35.112,30.873,31.557,32.75,26,32.969V33 c2.6,0,5,3.9,5,6.6V45c0,0.823,0.498,1.53,1.209,1.836C41.37,43.804,48,35.164,48,25C48,12.318,37.683,2,25,2S2,12.318,2,25 C2,35.164,8.63,43.804,17.791,46.836z"></path>
+                        </svg>
+                    </div>
+                    <div class="description">
+                        <h3>Github</h3>
+                        <a href="https://github.com/TygoWolven">TygoWolven</a>
+                    </div>
+                </div>
+            </label>
+        </div>
+    </div>
 </section>
 
 
 <style>
-    #contact > img:first-of-type {
-        animation: minecartDriving 15s linear infinite;
-    }
-
-    @keyframes minecartDriving {
-        0% {
-            transform: translateX(-100%);
-        }   
-        100% {
-            transform: translateX(100%);
-        }
-    }
-
     section {
         width: 100%;
         height: 100vh;
@@ -144,5 +174,88 @@
         100% {
             transform: translateY(0);
         }
+    }
+
+    .container {
+        height: 80px;
+        display: flex;
+        flex-wrap: nowrap;
+    }
+
+    .card {
+        width: 80px;
+        border-radius: .75rem;
+        background-size: cover;
+        cursor: pointer;
+        overflow: hidden;
+        border-radius: 2rem;
+        margin: 0 10px;
+        display: flex;
+        align-items: flex-end;
+        transition: .5s cubic-bezier(.28,-0.03,0,.99);
+        box-shadow: 0px 10px 30px -5px rgba(0, 0, 0, 0.8);
+    }
+
+    .card > .row {
+        color: white;
+        display: flex;
+        flex-wrap: nowrap;
+    }   
+
+    .card > .row > .icon {
+        background: white;
+        color: black;
+        border-radius: 50%;
+        width: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 15px;
+    }
+
+    .card > .row > .description {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        overflow: hidden;
+        height: 80px;
+        width: 520px;
+        opacity: 0;
+        transform: translateY(30px);
+        transition-delay: .3s;
+        transition: all .3s ease;
+    }
+
+    .description a {
+        color: rgb(200, 200, 200);
+        padding-top: 5px;
+    }
+
+    .description h3 {
+        color: white;
+        text-transform: uppercase;
+    }
+
+    input {
+        display: none;
+    }
+
+    input:checked + label {
+        width: 600px;
+    }
+
+    input:checked + label .description {
+        opacity: 1 !important;
+        transform: translateY(0) !important;
+    }
+
+    .card[for="c1"] {
+        background-image: url(/src/lib/assets/background1.png);
+    }
+    .card[for="c2"] {
+        background-image: url(/src/lib/assets/background2.png);
+    }
+    .card[for="c3"] {
+        background-image: url(/src/lib/assets/background3.png);
     }
 </style>
